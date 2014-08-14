9715c0b6b7f95f8fad3583986072150e5ce03492
bunneydude <mark.bunneyjr@gmail.com>
2014-08-14 00:26:35 -0700

    created schematic

---
+++ geda_files/ssmu.cmd
@@ -0,0 +1,84 @@
+# Pin name action command file
+
+# Start of element J1
+ChangePinName(J1, 2, 2)
+ChangePinName(J1, 1, 1)
+
+# Start of element J2
+ChangePinName(J2, 2, 2)
+ChangePinName(J2, 1, 1)
+
+# Start of element R?
+ChangePinName(R?, 1, 1)
+ChangePinName(R?, 2, 2)
+
+# Start of element D?
+ChangePinName(D?, 2, 2)
+ChangePinName(D?, 1, 1)
+
+# Start of element H1/U5
+ChangePinName(H1/U5, 4, V-)
+ChangePinName(H1/U5, 3, V+)
+ChangePinName(H1/U5, 5, out)
+ChangePinName(H1/U5, 2, in-)
+ChangePinName(H1/U5, 1, in+)
+
+# Start of element H1/U4
+ChangePinName(H1/U4, 4, V-)
+ChangePinName(H1/U4, 3, V+)
+ChangePinName(H1/U4, 5, out)
+ChangePinName(H1/U4, 2, in-)
+ChangePinName(H1/U4, 1, in+)
+
+# Start of element H1/U3
+ChangePinName(H1/U3, 4, V-)
+ChangePinName(H1/U3, 3, V+)
+ChangePinName(H1/U3, 5, out)
+ChangePinName(H1/U3, 2, in-)
+ChangePinName(H1/U3, 1, in+)
+
+# Start of element H1/U2
+ChangePinName(H1/U2, 4, V-)
+ChangePinName(H1/U2, 3, V+)
+ChangePinName(H1/U2, 5, out)
+ChangePinName(H1/U2, 2, in-)
+ChangePinName(H1/U2, 1, in+)
+
+# Start of element H1/R6
+ChangePinName(H1/R6, 1, 1)
+ChangePinName(H1/R6, 2, 2)
+
+# Start of element H1/R5
+ChangePinName(H1/R5, 1, 1)
+ChangePinName(H1/R5, 2, 2)
+
+# Start of element H1/R4
+ChangePinName(H1/R4, 1, 1)
+ChangePinName(H1/R4, 2, 2)
+
+# Start of element H1/R1
+ChangePinName(H1/R1, 1, 1)
+ChangePinName(H1/R1, 2, 2)
+
+# Start of element H1/U1
+ChangePinName(H1/U1, 4, V-)
+ChangePinName(H1/U1, 3, V+)
+ChangePinName(H1/U1, 5, out)
+ChangePinName(H1/U1, 2, in-)
+ChangePinName(H1/U1, 1, in+)
+
+# Start of element H1/R3
+ChangePinName(H1/R3, 1, 1)
+ChangePinName(H1/R3, 2, 2)
+
+# Start of element H1/R2
+ChangePinName(H1/R2, 1, 1)
+ChangePinName(H1/R2, 2, 2)
+
+# Start of element H1/C1
+ChangePinName(H1/C1, 2, 2)
+ChangePinName(H1/C1, 1, 1)
+
+# Start of element H1/D1
+ChangePinName(H1/D1, 2, 2)
+ChangePinName(H1/D1, 1, 1)
