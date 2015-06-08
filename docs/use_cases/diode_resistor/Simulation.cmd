5f9b7edb58e5b61ca049da077bb50463010a6b98
BunneyDude <mark.bunneyjr@gmail.com>
2015-06-07 17:44:47 -0700

    added raw data and image output

--- docs/use_cases/diode_resistor/Simulation.cmd
+++ docs/use_cases/diode_resistor/Simulation.cmd
@@ -1,4 +1,10 @@
-.OP
- .DC VINPUT 0 5 .01
+* ngspice -b spice.net
+
+.control
+op
+DC VINPUT 0 5 0.1
+hardcopy plot.ps vinput#branch vdummy#branch
+wrdata vout_vin v(Vin) v(Vout) i(VDUMMY)
+.endc
+
 .plot dc v(Vout) v(Vin)
-.print dc v(Vout) v(Vin)
