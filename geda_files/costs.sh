2f1ae67f5de8a6d28ce34c177e935b805bd6307e
BunneyDude <mark.bunneyjr@gmail.com>
2015-06-15 22:28:30 -0700

    Added unit group in costs script

--- geda_files/costs.sh
+++ geda_files/costs.sh
@@ -6,6 +6,26 @@ echo "Total cost of ssmu.sch"
 awk '{if(NR!=1) {print $4}}' bom_ssmu.txt | grep -v '?' | grep -v 'unknown' | awk '{s+=$1} END {print s}'
 echo ""
 
+echo "ssmu: adc_amp unit"
+grep 'adc_amp' bom_ssmu.txt | awk '{s+=$4} END {print s}'
+
+echo "ssmu: power unit"
+grep 'power' bom_ssmu.txt | awk '{s+=$4} END {print s}'
+
+echo "ssmu: mech unit"
+grep 'mech' bom_ssmu.txt | awk '{s+=$4} END {print s}'
+
+echo "ssmu: current_sense unit"
+grep 'current_sense' bom_ssmu.txt | awk '{s+=$4} END {print s}'
+
+echo "ssmu: uc unit"
+grep 'uc' bom_ssmu.txt | awk '{s+=$4} END {print s}'
+
+echo "ssmu: current_source unit"
+grep 'current_source' bom_ssmu.txt | awk '{s+=$4} END {print s}'
+
+
+
 echo "Total cost of adc_amp.sch"
 awk '{if(NR!=1) {print $4}}' bom_adc_amp.txt | grep -v '?' | grep -v 'unknown' | awk '{s+=$1} END {print s}'
 echo ""
@@ -13,3 +33,4 @@ echo ""
 echo "Total cost of dac_amp.sch"
 awk '{if(NR!=1) {print $4}}' bom_dac_amp.txt | grep -v '?' | grep -v 'unknown' | awk '{s+=$1} END {print s}'
 echo ""
+
